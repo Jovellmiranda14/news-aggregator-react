89ffac3c983e2450bf9aed1e5601baf544724eb7
Jovellmiranda <142292023+Jovellmiranda@users.noreply.github.com>
2025-04-06 21:54:31 +0800

    revert back to old files

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from "react";
-import { Container, Spinner, Row } from "react-bootstrap";
-import NewsList from "../components/NewsList";
+// import React, { useEffect, useState } from "react";
+// import { Container, Spinner, Row } from "react-bootstrap";
+// import NewsList from "../components/NewsList";
 
-export default function HomePage() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+// export default function HomePage() {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const getHomeNews = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(`/api/news`);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setArticles(data.articles.slice(0, 12));
-      } catch (error) {
-        console.error("Error fetching home news:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getHomeNews();
-  }, []);
+//   useEffect(() => {
+//     const getHomeNews = async () => {
+//       setLoading(true);
+//       try {
+//         const response = await fetch(`/api/news`);
+//         if (!response.ok) {
+//           throw new Error(`HTTP error! status: ${response.status}`);
+//         }
+//         const data = await response.json();
+//         setArticles(data.articles.slice(0, 12));
+//       } catch (error) {
+//         console.error("Error fetching home news:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//     getHomeNews();
+//   }, []);
 
-  return (
-    <Container className="my-4 d-flex flex-column align-items-center">
-      <Row>
-        {loading ? (
-          <div className="text-center my-4">
-            <Spinner animation="border" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </Spinner>
-          </div>
-        ) : (
-          <>
-            <h2 className="text-center">Newspaper Website</h2>
-            <p className="text-center">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-            <NewsList articles={articles} category="General" />
-          </>
-        )}
-      </Row>
-    </Container>
-  );
-}
+//   return (
+//     <Container className="my-4 d-flex flex-column align-items-center">
+//       <Row>
+//         {loading ? (
+//           <div className="text-center my-4">
+//             <Spinner animation="border" role="status">
+//               <span className="visually-hidden">Loading...</span>
+//             </Spinner>
+//           </div>
+//         ) : (
+//           <>
+//             <h2 className="text-center">Newspaper Website</h2>
+//             <p className="text-center">
+//               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+//               eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//             </p>
+//             <NewsList articles={articles} category="General" />
+//           </>
+//         )}
+//       </Row>
+//     </Container>
+//   );
+// }
