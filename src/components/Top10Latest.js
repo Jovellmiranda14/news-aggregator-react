d2ada1b11d5650799990d3efce8214bb0a38bdf0
Jovellmiranda <142292023+Jovellmiranda@users.noreply.github.com>
2025-06-10 12:04:03 +0800

    Refactor loading state rendering in Top10Latest component for improved clarity

--- src/components/Top10Latest.js
+++ src/components/Top10Latest.js
@@ -39,43 +39,42 @@ export default function Top10Latest() {
     getLatestNews();
   }, []);
 
-if (loading) {
+  if (loading) {
+    return (
+      <div className="text-center my-4 px-0" style={{ paddingLeft: "1rem" }}>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
+
   return (
-    <div className="text-center my-4 px-0" style={{ paddingLeft: "1rem" }}>
-      <Spinner animation="border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
+    <div className="mt-4 px-0" style={{ marginRight: "-1.5rem" }}>
+      <h4>Top Ten Topics</h4>
+      <ListGroup className="gap-1 d-flex flex-column">
+        {articles.map((article, index) => (
+          <ListGroup.Item key={index} className="border mb-2 gap-2">
+            <Link
+              to={`/article?url=${encodeURIComponent(article.url)}`}
+              className="text-dark"
+              style={{ textDecoration: "none" }}
+              state={{ article }}
+            >
+              <h1
+                className="mb-1"
+                style={{
+                  fontSize: "18px",
+                  whiteSpace: "normal",
+                  wordBreak: "break-word",
+                }}
+              >
+                {article.title}
+              </h1>
+            </Link>
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
-}
-
-return (
-  <div className="mt-4 px-0" style={{ marginRight: "-1.5rem" }}>
-    <h4>Top Ten Topics</h4>
-    <ListGroup className="gap-1 d-flex flex-column">
-      {articles.map((article, index) => (
-        <ListGroup.Item key={index} className="border mb-2 gap-2">
-          <Link
-            to={`/article?url=${encodeURIComponent(article.url)}`}
-            className="text-dark"
-            style={{ textDecoration: "none" }}
-            state={{ article }}
-          >
-            <h1
-              className="mb-1"
-              style={{
-                fontSize: "18px",
-                whiteSpace: "normal",
-                wordBreak: "break-word",
-              }}
-            >
-              {article.title}
-            </h1>
-          </Link>
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
-  </div>
-);
-
 }
